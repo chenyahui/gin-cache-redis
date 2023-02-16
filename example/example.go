029864b8c187d637e31a48fedf9b2c0061994ebd
chenyahui <cyhone@qq.com>
2023-02-16 18:11:34 +0800

    feat: add example

---
+++ example/example.go
@@ -0,0 +1,28 @@
+package example
+
+import (
+	cache "github.com/chenyahui/gin-cache"
+	redisbackend "github.com/chenyahui/gin-cache-redis"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9"
+	"time"
+)
+
+func main() {
+	app := gin.New()
+
+	redisStore := redisbackend.NewRedisV9Store(redis.NewClient(&redis.Options{
+		Network: "tcp",
+		Addr:    "127.0.0.1:6379",
+	}))
+
+	app.GET("/hello",
+		cache.CacheByRequestURI(redisStore, 2*time.Second),
+		func(c *gin.Context) {
+			c.String(200, "hello world")
+		},
+	)
+	if err := app.Run(":8080"); err != nil {
+		panic(err)
+	}
+}
